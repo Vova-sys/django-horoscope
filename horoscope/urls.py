219fad7456e152af5111486cc68c25846df1712e
Vova-sys <d3dkof@gmail.com>
2022-03-09 19:28:12 +0100

    function reverse (horname)

--- horoscope/urls.py
+++ horoscope/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:sign_zodiac>/', views.get_info_about_sign_zodiac_by_number), # конвертор str, int
-    path('<str:sign_zodiac>/', views.get_info_about_sign_zodiac),
+    path('<int:sign_zodiac>/', views.get_info_about_sign_zodiac_by_number),  # конвертор str, int
+    path('<str:sign_zodiac>/', views.get_info_about_sign_zodiac, name='horname'),
     # path('scorpio', views.scorpio),
     # path('aries', views.aries),
     # path('taurus', views.taurus),
