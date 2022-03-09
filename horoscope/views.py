76f8028fa49a613b72631648e75a974f2cbfda1b
Vova-sys <d3dkof@gmail.com>
2022-03-09 19:02:00 +0100

    HttpResponseRedirect url

--- horoscope/views.py
+++ horoscope/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 
 # Create your views here.
 
@@ -28,7 +28,11 @@ def get_info_about_sign_zodiac(request, sign_zodiac: str):
 
 
 def get_info_about_sign_zodiac_by_number(request, sign_zodiac: int):
-    return HttpResponse(f'This is number{sign_zodiac}')
+    zodiacs = list(zodiac_dict)
+    if sign_zodiac > len(zodiacs):
+        return HttpResponseNotFound(f'Неправильный порядковый номер знака зодиака - {sign_zodiac}')
+    name_zodiac = zodiacs[sign_zodiac-1]
+    return HttpResponseRedirect(f'/horoscope/{name_zodiac}')
 
 
 
