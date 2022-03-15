370dd22361c66ea644eb46d6c97a07397142bfe2
Vova-sys <d3dkof@gmail.com>
2022-03-15 23:28:48 +0100

    Django filters : title, add, capfirst

--- horoscope/views.py
+++ horoscope/views.py
@@ -64,12 +64,14 @@ class Person:
     def __str__(self):
         return f'This is {self.name}'
 
+
 def get_info_about_sign_zodiac(request, sign_zodiac: str):
     description = zodiac_dict.get(sign_zodiac)
     data = {
         'description_zodiac': description,
-        'sign': sign_zodiac.title(),
-        'my_class' : Person('Will', 55)
+        'sign': sign_zodiac,
+        'my_class': Person('Will', 55),
+        'my_int': 100
     }
     return render(request, 'horoscope/info_zodiac.html', context=data)
 
